a632f888bcbfc3d4122e3d3a3682b7d205d6766c
Pankz <pankznegi4638@gmail.com>
2023-03-27 11:32:30 +0530

    Moving Business logics from @Controller to @service

--- src/main/java/com/pankz/springboot/topic/TopicController.java
+++ src/main/java/com/pankz/springboot/topic/TopicController.java
@@ -1,5 +1,6 @@
 package com.pankz.springboot.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,13 @@ import java.util.List;
 @RestController
 
 public class TopicController {
+    @Autowired
+    private TopicService topicService;
     @RequestMapping("/topics")
     public List<Topics> getAllTopics() {
-        return Arrays.asList(
-                new Topics("spring"," Spring Framework","Spring Framework Description"),
-                new Topics("java"," Core Java","Core Java Description"),
-                new Topics("javascript"," JS ","JS Description")
+        return topicService.getAlltopics();
 
-        );
-                //Whatever be the return type(return object) Spring MVC's @RestController converted automatically in to JSON AND SENT BACK AS HTTP response
     }
 
 }
+//Whatever be the return type(return object) Spring MVC's @RestController converted automatically in to JSON AND SENT BACK AS HTTP response
